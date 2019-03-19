bbb9508e7511d15e1b446d0a604be6af6849aecb
jihyegnb <jihyegnb@naver.com>
2019-03-20 00:43:38 +0900

    add c++, java

--- coding/runFile.py
+++ coding/runFile.py
@@ -11,9 +11,11 @@ def runfile(languages):
         subprocess.call(["gcc", "coding/content.c", "-o", "content"])
         result = subprocess.check_output(["./content"])
     elif languages == 3:
-        result = "now preparing..."
+        subprocess.call(["javac", "-d", ".", "coding/content.java"])
+        result = subprocess.check_output(["java", "-cp", ".", "content"])
     else:
-        result = "now preparing..."
+        subprocess.call(["g++", "coding/content.cpp", "-o", "content2"])
+        result = subprocess.check_output(["./content2"])
 
     return result
 
